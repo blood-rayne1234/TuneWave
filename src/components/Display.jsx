1c0af9f103cb5527d173b703b86c6e9e25bcd58f
Akanksha Yadav <akankshay27@gmail.com>
2025-02-27 15:36:56 +0530

    test commit2

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -22,6 +22,10 @@ useEffect(()=>{
     }
 })
 
+
+
+
+
   return (
     <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
